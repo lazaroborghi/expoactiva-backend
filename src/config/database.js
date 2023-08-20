41ba82837ec72e145f911022360493351f4bcb98
unknown <lazaro.borghi1@gmail.com>
2023-08-20 20:27:36 -0300

    Actualizacion a gc secret manager

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,28 @@
 import mongoose from 'mongoose';
+import {SecretManagerServiceClient} from '@google-cloud/secret-manager';
 
-const MONGODB_URI = process.env.MONGODB_URI
-console.log(process.env)
+const SECRET_NAME = 'MONGODB_URI';
+const SECRET_VERSION = 'latest';
 
 class Database {
     constructor() {
         this.connection = null;
+        this.secretClient = new SecretManagerServiceClient();
+    }
+
+    async getMongoDbUriFromSecretManager() {
+        const [version] = await this.secretClient.accessSecretVersion({
+            name: `projects/${process.env.GOOGLE_CLOUD_PROJECT}/secrets/${SECRET_NAME}/versions/${SECRET_VERSION}`
+        });
+
+        // Obtiene el secreto y lo convierte en string.
+        return version.payload.data.toString('utf8');
     }
 
     async connect() {
         if (!this.connection) {
             try {
+                const MONGODB_URI = await this.getMongoDbUriFromSecretManager();
                 console.log("URI de MongoDB: ", MONGODB_URI)
                 this.connection = await mongoose.connect(MONGODB_URI, {
                     useNewUrlParser: true,
