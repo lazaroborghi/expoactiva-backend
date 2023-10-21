9369fb9d222befa241dfc2f109096a64bf2e1883
unknown <lazaro.borghi1@gmail.com>
2023-10-21 14:59:03 -0300

    guardando time to send notification v2

--- src/models/UserEvent.js
+++ src/models/UserEvent.js
@@ -29,14 +29,19 @@ const UserEventSchema = new mongoose.Schema(
 );
 
 UserEventSchema.pre('save', function(next) {
-    if (!this.timeToSendNotification) {
-      // Si eventStartTime está en UTC, convierte a UTC-3 restando 3 horas
-      let eventTimeUTC3 = new Date(this.eventStartTime.getTime() - (3 * 3600 * 1000));
-      
-      // Establecer el tiempo de envío de la notificación 15 minutos antes del inicio del evento (en UTC-3)
-      this.timeToSendNotification = new Date(eventTimeUTC3.getTime() - (15 * 60000));
+    // Verifica si eventStartTime está definido y si el documento es nuevo o si eventStartTime ha sido modificado.
+    if (this.isNew || this.isModified('eventStartTime')) {
+        // eventStartTime está en UTC, convierte a UTC-3 restando 3 horas
+        this.eventStartTime = new Date(this.eventStartTime.getTime() - (3 * 3600 * 1000));
     }
+
+    if (!this.timeToSendNotification) {
+        //resta 15 minutos
+        this.timeToSendNotification = new Date(this.eventStartTime.getTime() - (15 * 60000));
+    }
+
     next();
-  });  
+});
+ 
 
 export default mongoose.model('UserEvent', UserEventSchema);
