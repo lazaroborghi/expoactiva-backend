a80c7291f0092bbcb3d8982929575e4c3462dc23
unknown <lazaro.borghi1@gmail.com>
2023-10-21 13:11:00 -0300

    se prueba con UTC-3

--- src/services/cron/eventReminderCron.js
+++ src/services/cron/eventReminderCron.js
@@ -2,26 +2,32 @@ import expo from "../../config/expoInstance.js";
 import UserEvent from "../../models/UserEvent.js";
 import { Expo } from 'expo-server-sdk';
 
+import expo from "../../config/expoInstance.js";
+import UserEvent from "../../models/UserEvent.js";
+import { Expo } from 'expo-server-sdk';
+
 export const checkForUpcomingEvents = async () => {
     try {
         console.log('Buscando eventos que comenzarán en 15 minutos');
 
         // Obtener la fecha actual en UTC
         let now = new Date();
-        let currentDate = new Date(now.getTime() + now.getTimezoneOffset() * 60000);
 
-        // Calcular el tiempo de notificación (15 minutos después) en UTC
+        // Convertir a UTC-3, restar 3 horas (cada hora tiene 3600 segundos, por lo tanto 3 horas tienen 10800 segundos)
+        let currentDate = new Date(now.getTime() - (3 * 3600 * 1000)); 
+
+        // Calcular el tiempo de notificación (15 minutos después) en UTC-3
         let notificationTime = new Date(currentDate.getTime());
-        notificationTime.setUTCMinutes(currentDate.getUTCMinutes() + 15);
+        notificationTime.setMinutes(currentDate.getMinutes() + 15);
 
-        console.log('currentDate UTC: ', currentDate.toISOString());
-        console.log('notificationTime UTC: ', notificationTime.toISOString());
+        console.log('currentDate UTC-3: ', currentDate.toISOString());
+        console.log('notificationTime UTC-3: ', notificationTime.toISOString());
 
         // Buscar eventos que comenzarán en 15 minutos
         const events = await UserEvent.find({
           eventStartTime: {
-            $gte: currentDate,
-            $lt: notificationTime
+            $gte: currentDate.toISOString(),
+            $lt: notificationTime.toISOString()
           }
         });
 
