e7f18b388ed5129efafae25da8848528a9fc7c8a
unknown <lazaro.borghi1@gmail.com>
2023-11-14 17:34:15 -0300

    errorv2

--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -123,5 +123,8 @@ export const updateTicket = async function (req, res) {
         if (ticket) { res.status(200).json(ticket); }
         else { res.status(404).json({ error: "Entrada no encontrada" }); }
 
-    } catch (err) { res.status(500).json({ error: err.message }); }
+    } catch (err) { 
+        console.log(err)
+        res.status(500).json({ error: err.message }); 
+    }
 };
